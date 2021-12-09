5d6407a880da491645426d91132e8e0b24daccdb
Brian McCarthy <mccart71@purdue.edu>
2021-12-09 17:06:04 -0500

    Day 4 1

--- Day4Num1.jl
+++ Day4Num1.jl
@@ -26,24 +26,64 @@ function findBingoBoard( x, M )
 
     numBoards = length(M[:,1,1]);
     numNumbers = length(x);
+    chkNone = 0;
+    diffBoard = zeros(Int64, 5,5 );
 
     for i = 1:numBoards
         for j = 1:numNumbers
-            diffBoard = M[i,:,:] .- x[j];
-            chkRows = sum(diffBoard,dims=1);
-            chkCols = sum(diffBoard,dims=2);
+            diffBoard = diffBoard + ( M[i,:,:] .== x[j] )
+        end
 
-            if ( sum( chkRows .== 1 ) == 5 )
+
+        chkRows = sum(diffBoard,dims=1);
+        chkCols = sum(diffBoard,dims=2);
+
+        for k = 1:5
+            if ( chkRows[k] == 5 )
                 return i;
+                chkNone = 1;
                 break;
             end
-            if ( sum( chkCols .== 1 ) == 5 )
+            if ( chkCols[k] == 5 )
                 return i;
+                chkNone = 1;
                 break;
             end
         end
+    
+
+        diffBoard = zeros(Int64, 5,5 );
+
+        if chkNone == 1
+            break;
+        end
     end
 
-    return 0;
+    if chkNone == 0 
+        return 0;
+    end
 
+end
+
+
+boardIdx = findBingoBoard( nums[1:34], M )
+
+function sumUnusedNumbers( x, M )
+    
+    numNumbers = length(x);
+    diffBoard = zeros(Int64, 5,5 );
+    sumNums = 0;
+    
+    for j = 1:numNumbers
+        diffBoard = diffBoard + ( M .== x[j] )
+    end
+
+    for i = 1:5
+        for k = 1:5
+            if ( diffBoard[i,k] != 1 )
+                sumNums = sumNums + M[i,k];
+            end
+        end
+    end
+    return sumNums;
 end
